fc82e62236705f09e4a542cf9a8be41e43faba2d
ChangweiLi <1369990551@qq.com>
2018-06-23 16:12:03 +0800

    init

--- StudentInfo/views.py
+++ StudentInfo/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 # from django.http import request
 # Create your views here.
 def index(request):
-    hh = "Hello World!"
-    return render(request, 'index.html', hh)
+    return render(request, 'index.html')
--- StudentInfoWebApp/settings.py
+++ StudentInfoWebApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'StudentInfo'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'StudentInfoWebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_student',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
