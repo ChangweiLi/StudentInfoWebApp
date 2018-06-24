bdbf4c70e8cdf176b2bb315e29242796816e3a74
lichangwei <lichangwei_work@163.com>
2018-06-24 21:10:55 +0800

    添加增加学生页面，以及数据库验证成功

--- StudentInfo/views.py
+++ StudentInfo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from StudentInfo.models import Student,OneClass
 
 
 # from django.http import request
@@ -8,16 +9,25 @@ def index(request):
 
 
 def add(request):
+    name = request.GET.get('name')
+    age = request.GET.get('age')
+    learn = request.GET.get('learn')
+    Student.objects.get_or_create(name=name, age=age, learn=learn)
+    # print(name)
+    # print(age)
     return render(request, 'add.html')
 
 
 def delete(request):
+
     return render(request, 'delete.html')
 
 
 def query(request):
+
     return render(request, 'query.html')
 
 
 def update(request):
+
     return render(request, 'update.html')
