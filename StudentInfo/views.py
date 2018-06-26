f4bdb26db6256bb96327ee70d4ca2c92754d8bdd
ChangweiLi <1369990551@qq.com>
2018-06-26 13:57:53 +0800

    更新，删除页面都已经添加，若查询无记录则会返回＂无此记录＂

--- StudentInfo/views.py
+++ StudentInfo/views.py
@@ -25,18 +25,38 @@ def delete(request):
     name = request.GET.get('name')
     type1 = request.GET.get('type1')
     # type 为 1 表示执行数据库查询操作
-    if type1 == '1':
-        # 查询所在记录
-        try:
-            stu: Student = Student.objects.filter(name=name).get()
-        except Exception:
-            return HttpResponse("无此记录")
-        return render(request, 'delete.html', {'stu': stu})
+    # if type1 == '1':
+    #     # 查询所在记录
+    #     try:
+    #         stu: Student = Student.objects.filter(name=name).get()
+    #     except Exception:
+    #         return HttpResponse("无此记录")
+    #     return render(request, 'delete.html', {'stu': stu})
     # type 为 2 表示执行数据库确认删除操作
     if type1 == '2':
+        try:
+            Student.objects.filter(name=name).get()
+        except Exception:
+            return HttpResponse("无此记录")
         Student.objects.filter(name=name).delete()
-        return render(request, 'delete.html', {'stu': '删除成功'})
+        return render(request, 'delete.html', {'msg': '删除成功'})
     return render(request, 'delete.html')
+
+
+def update(request):
+    name = request.GET.get('name')
+    age = request.GET.get('age')
+    learn = request.GET.get('learn')
+    type1 = request.GET.get('type1')
+    # type 为1表示执行数据库操作，否则只是跳转页面
+    if type1 == '1':
+        stu = Student.objects.get(name=name)
+        stu.name = name
+        stu.age = age
+        stu.learn = learn
+        stu.save()
+        return render(request, "update.html", {'msg': '修改成功'})
+    return render(request, 'update.html')
 
 
 def query(request):
@@ -44,11 +64,14 @@ def query(request):
     type1 = request.GET.get('type1')
     # type 为1表示执行数据库操作，否则只是跳转页面
     if type1 == '1':
-        stu = Student.objects.get(name=name)  # 在student 表里新建一条记录
+        if name == 'all':
+            stu_list = Student.objects.all()
+            return render(request, 'query.html', {'stu_list': stu_list})
+        try:
+            Student.objects.filter(name=name).get()
+        except Exception:
+            return HttpResponse("无此记录")
+        stu = Student.objects.get(name=name)  # 在student 表里查询一条记录
         return render(request, 'query.html', {'stu': stu})
 
     return render(request, 'query.html')
-
-
-def update(request):
-    return render(request, 'update.html')
